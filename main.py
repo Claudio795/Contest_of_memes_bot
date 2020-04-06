92c7dc4d6a5ee2e72996323d8cb110ee5a295f1b
Claudio795 <claudiocurto@hotmai.it>
2020-04-06 09:49:26 +0200

    Final commit for v1.0

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 import string
 from pprint import pprint
 from config import token, my_chat_id
-#from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+
+# from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = telepot.Bot(token)
 
@@ -13,15 +14,17 @@ def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     username = msg["from"]["username"]
 
-#    keyboard = InlineKeyboardMarkup(
- #       inline_keyboard=[[InlineKeyboardButton(text="Pubblicato", callback_data='pubblicato' + str(chat_id))]])
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[[InlineKeyboardButton(text="Pubblicato", callback_data='pubblicato' + str(chat_id))]])
 
     if content_type == "text":
         txt = msg['text']
         if txt == '/start':
             bot.sendMessage(chat_id, "Benvenuto " + username + "! Invia qui i tuoi meme!")
         elif txt == '/help':
-            bot.sendMessage(chat_id, "Manda l'immagine che vuoi pubblicare, se accettata riceverai una notifica quando verrà pubblicata sul canale")
+            bot.sendMessage(chat_id,
+                            "Manda l'immagine che vuoi pubblicare, se accettata riceverai una notifica quando verrà "
+                            "pubblicata sul canale")
         else:
             bot.sendMessage(chat_id, "Al momento accetto solo immagini")
 
@@ -30,20 +33,26 @@ def on_chat_message(msg):
         bot.sendMessage(chat_id, "Grazie, il tuo meme è sotto esame...")
 
         bot.sendPhoto(my_chat_id, media_id)
-        bot.sendMessage(my_chat_id, "Ciao Claudio, @" + username + " ha inviato questo")
+        bot.sendMessage(my_chat_id, "Ciao Claudio, @" + username + " ha inviato questo", reply_markup=keyboard)
 
     else:
         bot.sendMessage(chat_id, "Al momento accetto solo immagini")
 
-
-#def on_callback_query(msg):
- #   query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-  #  chat_id = int(query_data[10:]0)
-
- #   if query_data[:10] == 'pubblicato':
-  #      bot.sendMessage(chat_id, "Pubblicato!")
+    print("Listening...")
 
 
-bot.message_loop(on_chat_message)
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    chat_id = int(query_data[10:])
+
+    if query_data[:10] == 'pubblicato':
+        bot.answerCallbackQuery(query_id, text='Ok')
+        bot.sendMessage(chat_id, "Pubblicato!")
+
+    print("Waiting...")
+
+
+bot.message_loop({'chat': on_chat_message,
+                  'callback_query': on_callback_query})
 while 1:
-    time.sleep(2)
+    time.sleep(10)
